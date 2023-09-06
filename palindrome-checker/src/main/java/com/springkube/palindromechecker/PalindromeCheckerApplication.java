f2d17c3a8a21848ed8bab0e0ef9f8172fc327abe
sovisrushain <sovisrushain@gmail.com>
2023-09-06 15:14:25 +0530

    initalize the palindrome-service

---
+++ palindrome-checker/src/main/java/com/springkube/palindromechecker/PalindromeCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.springkube.palindromechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromeCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindromeCheckerApplication.class, args);
+	}
+
+}
---
+++ palindrome-checker/src/test/java/com/springkube/palindromechecker/PalindromeCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springkube.palindromechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalindromeCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
