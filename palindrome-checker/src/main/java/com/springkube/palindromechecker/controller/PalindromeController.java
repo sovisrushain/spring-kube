b7a5fb8f7a197b9745bd4373875f59fc1ed44f65
sovisrushain <sovisrushain@gmail.com>
2023-10-05 13:22:54 +0530

    config logs for elk stack

--- palindrome-checker/src/main/java/com/springkube/palindromechecker/controller/PalindromeController.java
+++ palindrome-checker/src/main/java/com/springkube/palindromechecker/controller/PalindromeController.java
@@ -1,8 +1,8 @@
 package com.springkube.palindromechecker.controller;
 
 import com.springkube.palindromechecker.service.PalindromeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +17,7 @@ public class PalindromeController {
 
     private final PalindromeService palindromeService;
 
-    private static final Logger logger = LoggerFactory.getLogger(PalindromeController.class);
+    private static final Logger logger = LogManager.getLogger(PalindromeController.class);
 
     @Autowired
     public PalindromeController(PalindromeService palindromeService) {
--- palindrome-checker/src/main/java/com/springkube/palindromechecker/service/PalindromeServiceImpl.java
+++ palindrome-checker/src/main/java/com/springkube/palindromechecker/service/PalindromeServiceImpl.java
@@ -1,7 +1,8 @@
 package com.springkube.palindromechecker.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.springkube.palindromechecker.controller.PalindromeController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Matcher;
@@ -13,7 +14,7 @@ public class PalindromeServiceImpl implements PalindromeService {
     private static final String isPalindrome = "Given String is a Palindrome";
     private static final String isNotPalindrome = "Given String is not a Palindrome";
 
-    private static final Logger logger = LoggerFactory.getLogger(PalindromeServiceImpl.class);
+    private static final Logger logger = LogManager.getLogger(PalindromeController.class);
 
     @Override
     public String checkPalindrome(String string) {
